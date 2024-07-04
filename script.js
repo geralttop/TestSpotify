10a6e4d904dbf52d52d6d649f417eb197b6f9510
geralttop <bluevotinamalibu@gmail.com>
2024-07-04 18:50:06 +0300

    Добавил тут  немного для теста

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const client_id = 'b1f2f839bee74b818813d23dfe33a048'
 const client_secret = '850cb5d402fa40dba0d0079ee495f89c'
 let accessToken = '';
 
+//
+const liUserName = document.querySelector('.userName');
+const liUserFollowers = document.querySelector('.userFollowers')
+const buttonSP = document.querySelector(".showProfile");
 //Штука для получения токена
+
 async function getToken() {
     const response = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
@@ -19,7 +24,6 @@ async function getToken() {
     const data = await response.json();
     accessToken = data.access_token;
 }
-
 //штука для получения инфы о треке
 async function getTrackInfo(track_id) {
     const response = await fetch(`https://api.spotify.com/v1/tracks/${track_id}`, {
@@ -39,17 +43,31 @@ async function getArtistInfo(artist_id) {
     return await response.json();
 }
 
+
 async function logTrackInfo(artist_id){
-    await getToken();
+
 
     let data = await getArtistInfo(artist_id)
     console.log(data);
 }
 
-logTrackInfo('5WoWlP0ihSFIxnppxjwSgE')
-// а это печать инфы о треке
-// getToken().then(response => {
-//     getTrackInfo(response.access_token, '3PLiVjlq6IgsWSe2iUyVhZ').then(profile => {
-//       console.log(profile)
-//     })
-//   });
+async function getUserProfile(username){
+    const response = await fetch(`https://api.spotify.com/v1/users/${username}`,{
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + accessToken }
+    });
+
+    return await response.json();
+}
+
+async function showUserProfile(){
+    await getToken();
+    let userID = document.querySelector('.inpProfile')
+    let user = await getUserProfile(userID.value)
+    liUserName.textContent += user.display_name;
+    liUserFollowers.textContent += user.followers.total;
+}
+
+buttonSP.addEventListener("click", ()=>{
+    showUserProfile()
+})
